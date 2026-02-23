2955cb2b44a33a2209c4ada9c249c50bf73efbe2
David Monaghan <monaghan.david@gmail.com>
2026-02-23 13:37:53 -0500

    Enhance autofill styles for input and textarea elements for better visibility and user experience

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -246,14 +246,29 @@
 	input:-webkit-autofill,
 	input:-webkit-autofill:hover,
 	input:-webkit-autofill:focus,
+	input:-webkit-autofill:active,
 	textarea:-webkit-autofill,
 	textarea:-webkit-autofill:hover,
-	textarea:-webkit-autofill:focus {
-		-webkit-text-fill-color: #fff;
-		-webkit-box-shadow: 0 0 0px 1000px #000 inset;
-		box-shadow: 0 0 0px 1000px #000 inset;
-		transition: background-color 5000s ease-in-out 0s;
-		caret-color: #fff;
+	textarea:-webkit-autofill:focus,
+	textarea:-webkit-autofill:active {
+		-webkit-text-fill-color: #fff !important;
+		-webkit-box-shadow: 0 0 0px 1000px #000 inset !important;
+		box-shadow: 0 0 0px 1000px #000 inset !important;
+		background-color: #000 !important;
+		color: #fff !important;
+		transition: background-color 5000s ease-in-out 0s, color 5000s ease-in-out 0s;
+		caret-color: #fff !important;
+		-webkit-background-clip: text !important;
+		background-clip: text !important;
+	}
+
+	input:-internal-autofill-selected,
+	textarea:-internal-autofill-selected {
+		-webkit-text-fill-color: #fff !important;
+		-webkit-box-shadow: 0 0 0px 1000px #000 inset !important;
+		box-shadow: 0 0 0px 1000px #000 inset !important;
+		background-color: #000 !important;
+		color: #fff !important;
 	}
 
 	.error {
